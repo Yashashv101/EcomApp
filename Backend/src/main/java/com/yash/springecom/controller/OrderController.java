4264a4fe3a5787618ce1b9a56a339c7de43d2a62
Yashashv <yashashvsinghxia1397@gmail.com>
2026-01-15 00:01:28 +0530

    Added OrderController and OrderService to check if frontend is working

---
+++ Backend/src/main/java/com/yash/springecom/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.yash.springecom.controller;
+
+import com.yash.springecom.model.dto.OrderRequest;
+import com.yash.springecom.model.dto.OrderResponse;
+import com.yash.springecom.service.OrderService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class OrderController{
+    @Autowired
+    private OrderService service;
+
+    @PostMapping("/orders/place")
+    public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest){
+        OrderResponse orderResponse=service.placeOrder(orderRequest);
+        return new ResponseEntity<>(orderResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/orders")
+    public ResponseEntity<List<OrderResponse>> getAllOrders(){
+        List<OrderResponse> ls=service.getAllOrders();
+        return new ResponseEntity<>(ls,HttpStatus.OK);
+    }
+}
---
+++ Backend/src/main/java/com/yash/springecom/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.yash.springecom.service;
+
+import com.yash.springecom.model.dto.OrderRequest;
+import com.yash.springecom.model.dto.OrderResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+        return null;
+    }
+
+    public List<OrderResponse> getAllOrders() {
+        return null;
+    }
+}
